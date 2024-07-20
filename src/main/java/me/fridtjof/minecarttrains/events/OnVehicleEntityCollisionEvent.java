41789d499cbf8488a139f79591df2c91ded7fd4f
Wallaceman105 <wallaceman105@gmail.com>
2024-07-20 12:36:58 -0400

    Removing this entirely. It's fun but too risky for multiplayer. Might revisit.

--- src/main/java/me/fridtjof/minecarttrains/events/OnVehicleEntityCollisionEvent.java
+++ src/main/java/me/fridtjof/minecarttrains/events/OnVehicleEntityCollisionEvent.java
@@ -8,60 +8,60 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 
-public class OnVehicleEntityCollisionEvent implements Listener
-{
+//public class OnVehicleEntityCollisionEvent implements Listener
+//{
 
-    static MinecartTrains plugin = MinecartTrains.getInstance();
+//    static MinecartTrains plugin = MinecartTrains.getInstance();
 
     // killing entities when run over
-    @EventHandler
-    public void onVehicleEntityCollision(VehicleEntityCollisionEvent event)
-    {
+//    @EventHandler
+//    public void onVehicleEntityCollision(VehicleEntityCollisionEvent event)
+//    {
 
-        if(!plugin.configManager.mainConfig.getConfig().getBoolean("trains.run_over_entities"))
-        {
-            return;
-        }
+//        if(!plugin.configManager.mainConfig.getConfig().getBoolean("trains.run_over_entities"))
+//        {
+//            return;
+//        }
 
-        Vehicle vehicle = event.getVehicle();
-        Entity  entity = event.getEntity();
+//        Vehicle vehicle = event.getVehicle();
+//        Entity  entity = event.getEntity();
 
-        if(!(vehicle instanceof Minecart))
-        {
-            return;
-        }
-        if(entity instanceof Minecart)
-        {
-            return;
-        }
-        if(!(entity instanceof Damageable))
-        {
-            return;
-        }
+//        if(!(vehicle instanceof Minecart))
+//        {
+//            return;
+//        }
+//        if(entity instanceof Minecart)
+//        {
+//            return;
+//        }
+//        if(!(entity instanceof Damageable))
+//        {
+//            return;
+//        }
 
-        if(vehicle instanceof RideableMinecart)
-        {
-            if(vehicle.getPassenger() == null)
-            {
-                return;
-            }
-        }
+//        if(vehicle instanceof RideableMinecart)
+//        {
+//            if(vehicle.getPassenger() == null)
+//            {
+//                return;
+//            }
+//        }
 
         //fixing rare situations where a player riding the cart was killed
-        if(entity.isInsideVehicle())
-        {
-            return;
-        }
+//        if(entity.isInsideVehicle())
+//        {
+//            return;
+//        }
 
-        if(vehicle.getVelocity().length() < plugin.configManager.mainConfig.getConfig().getDouble("trains.run_over_entities_min_velocity"))
-        {
-            return;
-        }
+//        if(vehicle.getVelocity().length() < plugin.configManager.mainConfig.getConfig().getDouble("trains.run_over_entities_min_velocity"))
+//        {
+//            return;
+//        }
 
-        Damageable damageable = (Damageable) entity;
+//        Damageable damageable = (Damageable) entity;
         //damageable.setHealth(0); this also killed player in gamemode 1
-        damageable.damage(10000, vehicle);
+//        damageable.damage(10000, vehicle);
 
         //System.out.println("" + vehicle.getVelocity().length());
-    }
-}
+//    }
+//}
